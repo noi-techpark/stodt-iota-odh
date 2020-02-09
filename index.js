3ea336e5c6bab77e02acf291a3943200e25e1d78
huhn511 <sehe89@gmail.com>
2020-02-09 17:43:16 +0100

    add server setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Started.")
+const express = require('express')
+const app = express()
+const mamModule = require('./lib/mam-server')
+
+
+const startServer = () => {
+    let server = mamModule.createServer(app, {})
+
+    server.listen(3000, function() {
+        console.log(`Server started on http://localhost:3000 `)
+    })
+}
+
+
+// Run the service
+startServer();

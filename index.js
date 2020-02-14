d594db1abae76ebba9400844547f2a65a7e702de
huhn511 <sehe89@gmail.com>
2020-02-14 15:34:04 +0100

    add port and syncInterval as options

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const mamModule = require('./lib/mam-server')
 const synchronizer = require('./lib/synchronizer')
+const { port } = require('../config/options.json')
 
 
 const startServer = () => {
     let server = mamModule.createServer(app, {})
 
-    server.listen(3000, function() {
+    server.listen(port, function() {
         console.log(`Server started on http://localhost:3000 `)
         try {
             synchronizer.start();
